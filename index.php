98a3edb21f25c23bb1fb765ab882cfd8999940ff
Marco <marco.carone@libero.it>
2020-03-11 18:15:50 +0100

    up

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
   $dipendente_01 = new Dipendente( 1, "Marco", "Carone", "CF48574FHRH84P10DD", "Web Developer");
   $dipendente_01->setIndirizzo("via geranii,380 Honolulu");
   $dipendente_01->setStipendio(1500);
-
   $dipendente_01->setGiorniFerieUtilizzati(10);
   $dipendente_01->setGiorniFerieMaturati(30);
   var_dump($dipendente_01);

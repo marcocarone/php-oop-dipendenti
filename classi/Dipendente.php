bc00e0ba0aa7cdd5b9723c4c036b21cea90287c0
Marco <marco.carone@libero.it>
2020-03-11 16:01:42 +0100

    aggiornamento

--- classi/Dipendente.php
+++ classi/Dipendente.php
@@ -3,23 +3,42 @@ require_once __DIR__ . "/../traits/Ferie.php";
 
   class Dipendente
   {
-    public $id;
-    public $nome;
-    public $cognome;
-    public $cf;
-    public $ruolo;
-    public $indirizzo;
-    public $stipendio;
-    use Ferie;
+      protected $id;
+      protected $nome;
+      protected $cognome;
+      protected $cf;
+      protected $ruolo;
+      public $indirizzo;
+      public $stipendio;
+      use Ferie;
 
-    public function __construct($_id, $_nome, $_cognome, $_cf, $_ruolo)
-    {
-      $this->id = $_id;
-      $this->nome = $_nome;
-      $this->cognome = $_cognome;
-      $this->cf = $_cf;
-      $this->ruolo = $_ruolo;
-    }
+      public function __construct($_id, $_nome, $_cognome, $_cf, $_ruolo)
+      {
+          if (empty($_id) || !is_int($_id)) {
+              throw new Exception('Non è un numero');
+          } else {
+              $this->id = $_id;
+          }
+          if (empty($_nome)) {
+              throw new Exception('Campo non è settato');
+          } else {
+              $this->nome = $_nome;
+          }
+          if (empty($_cognome)) {
+              throw new Exception('Campo non è settato');
+          } else {
+              $this->cognome = $_cognome;
+          }
+          if (empty($_cf)) {
+              throw new Exception('Campo non è settato');
+          } else {
+              $this->cf = $_cf;
+          }
+          if (empty($_ruolo)) {
+              throw new Exception('Campo non è settato');
+          } else {
+              $this->ruolo = $_ruolo;
+          }
+
+      }
   }
-
- ?>

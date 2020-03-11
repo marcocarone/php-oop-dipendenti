184c032c0accf3ed909c6fc5df2cffaaade6c00d
Marco <marco.carone@libero.it>
2020-03-11 14:56:26 +0100

    inizio

---
+++ ideaprogetto.php
@@ -0,0 +1,11 @@
+//Crea e modellizza classi per gestire i dipendenti di un’azienda che produce gruppi elettrogeni.
+
+Dipendente
+  protected id
+  protected nome
+  protected cognome
+  protected cf
+  protected ruolo
+  protected    indirizzo
+  protected stipendio
+  protected giorniFerie
